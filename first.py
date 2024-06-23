ad38e2b395e2bfaf0a4d905fb2538da9c00f26b5
Eral02 <122754482+Eral02@users.noreply.github.com>
2024-06-23 17:38:05 +0500

    Assigment02

---
+++ first.py
@@ -0,0 +1,26 @@
+#area of triangle
+base=float(input("enter base: "))
+print(base)
+heigh=float(input("enter heigh: ")) 
+print(heigh)
+area=float(input("enter area: "))
+print(area)
+area_of_triangle=area*base*heigh
+print(area_of_triangle)
+
+#area of square
+width=int(input("enter width: "))
+print(width)
+length=int(input("enter length : ")) 
+print(length)
+area=width*length
+print(area)
+
+#area of circle
+circle=float(input("enter circle: "))
+print(circle)
+radius=int(input("enter radius : ")) 
+print(radius)
+area_of_circle=circle*radius*radius
+print(area_of_circle)
+
